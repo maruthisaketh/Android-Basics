8add110f5548b8c29646bb5df92f9904884e07c4
maruthisaketh <amsaket1998@gmail.com>
2024-02-11 19:07:03 -0600

    First Commit

---
+++ ExampleApplication/app/src/main/java/com/example/exampleapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.exampleapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickFunction(view: View) {
+        Log.i("Info","Button Clicked here")
+    }
+}
